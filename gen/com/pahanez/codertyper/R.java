f4d61c28269ce119cef6fe841aee819102894d60
pahanez <pahanez@gmail.com>
2013-12-02 00:20:53 +0300

    + Sliding Menu

--- gen/com/pahanez/codertyper/R.java
+++ gen/com/pahanez/codertyper/R.java
@@ -34,22 +34,22 @@ public final class R {
         public static final int title_cursor=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f0a0008;
-        public static final int content_frame=0x7f0a0004;
+        public static final int action_settings=0x7f0a0007;
         public static final int drawer_layout=0x7f0a0003;
-        public static final int hacker_et=0x7f0a0007;
-        public static final int hacker_typer_tv=0x7f0a0006;
+        public static final int hacker_et=0x7f0a0006;
+        public static final int hacker_typer_tv=0x7f0a0005;
         public static final int inner_fragment=0x7f0a0000;
-        public static final int left_drawer=0x7f0a0005;
+        public static final int left_drawer=0x7f0a0004;
         public static final int main_menu_title=0x7f0a0001;
         public static final int menu_start_button=0x7f0a0002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int main_menu=0x7f030001;
-        public static final int menu_layout=0x7f030002;
-        public static final int toast_text=0x7f030003;
-        public static final int typer_layout=0x7f030004;
+        public static final int menu_item=0x7f030002;
+        public static final int menu_layout=0x7f030003;
+        public static final int toast_text=0x7f030004;
+        public static final int typer_layout=0x7f030005;
     }
     public static final class menu {
         public static final int main=0x7f090000;
--- src/com/pahanez/codetyper/MainActivity.java
+++ src/com/pahanez/codetyper/MainActivity.java
@@ -2,16 +2,44 @@ package com.pahanez.codetyper;
 
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.pahanez.codertyper.R;
 
 public class MainActivity extends FragmentActivity {
+	
+private String[] mFileTitles = new String[]{"Mercury" , "Venera" , "Earth"};
+	
+	private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.menu_layout);
+		
+		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this,
+               R.layout.menu_item, mFileTitles));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+		
 		initMenuFrament();
+	}
+	
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+	    @Override
+	    public void onItemClick(AdapterView parent, View view, int position, long id) {
+	    	Log.e("p37td8" , "id " + id);
+	    }
 	}
 
 	private void initMenuFrament() {
