c8ef25cddd28985217d954bb057881f948573fbb
pahanez <pahanez@gmail.com>
2013-12-25 21:02:34 +0300

    gradient

---
+++ src/com/pahanez/codetyper/CustomView.java
@@ -0,0 +1,59 @@
+package com.pahanez.codetyper;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.pahanez.codertyper.R;
+
+public class CustomView extends View{ // DEL TODO
+	
+	private Handler mHandler = new Handler();
+	
+	private LinearGradient mLinearGradient;
+	
+	private Runnable mUpdater = new Runnable() {
+		
+		@Override
+		public void run() {
+			CustomView.this.invalidate();
+		}
+	};
+	
+	private final int mDisplayWidth;
+	private final int mDisplayHeight;
+	private int mYPosition;
+	private Paint mPaint = new Paint();
+
+	public CustomView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mDisplayHeight = context.getResources().getDisplayMetrics().heightPixels;
+		mDisplayWidth = context.getResources().getDisplayMetrics().widthPixels;
+		context.getResources().getColor(R.color.typer_color_green);
+		
+		mLinearGradient = new LinearGradient(
+			    0, 0, 0, 100, 
+			    Color.BLACK, Color.GREEN, Shader.TileMode.CLAMP);
+		mPaint.setShader(mLinearGradient);
+		
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+//		android.util.Log.e("p37td8", "customFrameLay");
+		
+		mHandler.postDelayed(mUpdater, 1000/25);
+		super.onDraw(canvas);
+		canvas.drawRect(0, (++mYPosition - 100), mDisplayWidth, mYPosition, mPaint);
+		if(mYPosition > (mDisplayHeight + 200)) mYPosition = -30;
+		
+	}
+
+}
--- src/com/pahanez/codetyper/FocusedEditText.java
+++ src/com/pahanez/codetyper/FocusedEditText.java
@@ -1,6 +1,9 @@
 package com.pahanez.codetyper;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.ViewDebug.ExportedProperty;
 import android.widget.EditText;
@@ -15,6 +18,11 @@ public class FocusedEditText extends EditText{
 	@ExportedProperty(category = "focus")
 	public boolean isFocused() {
 		return true;
+	}
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
 	}
 
 }
